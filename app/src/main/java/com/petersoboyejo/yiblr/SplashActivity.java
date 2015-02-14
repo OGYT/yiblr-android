52372e79f0f5d1b975da8027dd48ed9b500260e2
Peter Soboyejo <peter.soboyejo@aol.com>
2015-02-14 14:46:15 -0500

    Random Splash

--- app/src/main/java/com/petersoboyejo/yiblr/SplashActivity.java
+++ app/src/main/java/com/petersoboyejo/yiblr/SplashActivity.java
@@ -1,39 +1,31 @@
 package com.petersoboyejo.yiblr;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
+public class SplashActivity extends Activity {
 
-public class SplashActivity extends ActionBarActivity {
+    public static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-    }
 
+        new Handler().postDelayed(new Runnable() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
-        return true;
-    }
+            @Override
+            public void run() {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+                finish();
 
-        return super.onOptionsItemSelected(item);
+            }
+        }, SPLASH_TIME_OUT);
+
     }
 }
